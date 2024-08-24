ef655bdda55bc7ab61d4a40419052f46594f0504
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-08-24 22:21:44 +0530

    efficient

--- check_if_arr_sorted.cpp
+++ check_if_arr_sorted.cpp
@@ -15,7 +15,7 @@ bool isSorted(int arr[], int n){
 */
 
 
-// efficient
+// efficient: time complexity is O(n)
 int isSorted(int arr[], int n){
     for(int i=1; i<n; i++){
         if(arr[i] < arr[i-1]){
